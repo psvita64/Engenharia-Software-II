f7b27f7ba57ddd02d549407de92ab78d998f381c
IDarkNess <148880754+psvita64@users.noreply.github.com>
2026-03-13 18:01:23 +0000

    Delete Memento directory

--- Memento/ExistingStudentException.java
+++
@@ -1,7 +0,0 @@
-package com.es2.memento;
-
-public class ExistingStudentException extends RuntimeException {
-    public ExistingStudentException(String message) {
-        super(message);
-    }
-}
--- Memento/NotExistingSnapshotException.java
+++
@@ -1,7 +0,0 @@
-package com.es2.memento;
-
-public class NotExistingSnapshotException extends RuntimeException {
-    public NotExistingSnapshotException(String message) {
-        super(message);
-    }
-}
