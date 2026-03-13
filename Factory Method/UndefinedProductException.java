4210211b4ac3298c0550e04b7a592e5f78badc06
IDarkNess <148880754+psvita64@users.noreply.github.com>
2026-03-13 18:01:14 +0000

    Delete Factory Method directory

--- Factory Method/UndefinedProductException.java
+++
@@ -1,8 +0,0 @@
-package com.es2.factorymethod;
-
-public class UndefinedProductException extends Exception {
-
-    public UndefinedProductException() {
-        super("Undefined product type");
-    }
-}
